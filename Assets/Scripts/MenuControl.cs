46498b491bd879ca904484581c4ebbc7c6f4f270
mra <mra.app21@gmail.com>
2024-08-11 18:17:02 +0330

    feat(scene):add pause menu (no design)

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -6,10 +6,16 @@ using UnityEngine.SceneManagement;
 public class MenuControl : MonoBehaviour
 {
     public string SceneName;
+    public string MenuName;
     public void OnGameClick(int idx)
     {
 
         SceneManager.LoadScene(SceneName);
+    }
+    public void OnGoToMenuClick()
+    {
+
+        SceneManager.LoadScene(MenuName);
     }
     public void OnExitClicked()
     {
