a7ed5179577cd263ca524ff4afdd7d0bab148961
mra <mra.app21@gmail.com>
2024-09-24 17:35:15 +0330

    feat(Scene): add game soundtrack
    - in menu scene added OST GameObject with a dontdestroy on load script

---
+++ Assets/Scripts/common/DontDestroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDistroy : MonoBehaviour
+{
+    public static DontDistroy Instance;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance != null)
+            Destroy(gameObject);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
